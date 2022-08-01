4fffa628ed48588621035df1ed3a056264b99da4
laura422 <lsoria4420@gmail.com>
2022-08-01 14:16:40 +0200

    last

--- lenguajes/js.js
+++ lenguajes/js.js
@@ -23,3 +23,27 @@ console.log('El primer elemento del array es: ' + head(array));
 const tail = (_first, ...nums) => nums
 
 console.log('Devuelve todos los elemento menos el primero: ' + tail(0, 1, 2, 3));
+
+
+/**
+ * Init
+ * Implementa una función `init` (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
+ */
+
+const init = (nums) =>{
+
+nums.pop();
+return console.log(`Devuelve todos los elementos menos el último: ${nums}`);
+
+}
+
+init(array);
+
+/**
+ * Last
+ * Implementa una función `last` (inmutable), tal que, dado un array como entrada devuelva el último elemento.
+ */
+const arrayLast = ['primer', 'segundo', 'último'];
+const last = (nums) => nums.pop();
+
+console.log("Devuelve el último elemento: " + last(arrayLast));
