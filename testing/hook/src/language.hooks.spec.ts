a5d7b891d5a5d4b7d78388738c37a338efe033c5
laura422 <lsoria4420@gmail.com>
2023-01-04 09:39:58 +0100

    Terminado hook

---
+++ testing/hook/src/language.hooks.spec.ts
@@ -0,0 +1,21 @@
+import { renderHook, act } from '@testing-library/react';
+import { useLanguage } from './language.hooks';
+import { LanguageProvider } from './language.context';
+
+describe('useLanguage specs', () => {
+  it('should return a message with language equals "en" when it renders the hook', () => {
+    // Arrange
+
+    // Act
+    const { result } = renderHook(() => useLanguage(), {
+      wrapper: LanguageProvider,
+    });
+
+    act(() => {
+      result.current.setLanguage('en');
+    });
+
+    // Assert
+    expect(result.current.message).toEqual('The current language is: en');
+  });
+});
